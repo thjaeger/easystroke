30e405b7964e2b58a015d91c5834b8b3428a3d35
Tom Jaeger <ThJaeger@gmail.com>
2008-06-02 00:37:58 -0400

    prevent flickering when using Shape under compiz
    darcs-hash:20080602043758-a887e-4d248e5d351fea0ae80d657e7648b46816f97abf.gz

--- shape.h
+++ shape.h
@@ -1,6 +1,7 @@
 #ifndef __SHAPE_H__
 #define __SHAPE_H__
 #include "trace.h"
+#include <glibmm/thread.h>
 
 class Shape : public Trace {
 	Window win;
@@ -8,6 +9,8 @@ private:
 	virtual void draw(Point p, Point q);
 	virtual void start();
 	void clear();
+	void defer_clear();
+	Glib::Mutex clear_mutex;
 public:
 	Shape();
 	virtual void end();
