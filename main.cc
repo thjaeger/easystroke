7f42bde311052ffa237c22eaad79fc8709d0a0b1
ThJaeger <ThJaeger@gmail.com>
2008-09-18 20:39:02 -0400

    better debugging output
    darcs-hash:20080919003902-485ad-7bace8cce9d29a77cdc83d0c97d4ad5ea95eaeca.gz

--- main.cc
+++ main.cc
@@ -1361,7 +1361,7 @@ void Main::handle_event(XEvent &ev) {
 
 	case ButtonPress:
 		if (verbosity >= 3)
-			printf("Press: %d (%ld)\n", ev.xbutton.button, ev.xbutton.time);
+			printf("Press: %d (%d, %d)\n", ev.xbutton.button, ev.xbutton.x, ev.xbutton.y);
 		last_press_t = ev.xbutton.time;
 		if (last_e && ev.xmotion.time < last_e->t)
 			break;
@@ -1381,7 +1381,7 @@ void Main::handle_event(XEvent &ev) {
 
 	case ButtonRelease:
 		if (verbosity >= 3)
-			printf("Release: %d\n", ev.xbutton.button);
+			printf("Release: %d (%d, %d)\n", ev.xbutton.button, ev.xbutton.x, ev.xbutton.y);
 		last_press_t = 0;
 		if (last_e && ev.xmotion.time < last_e->t)
 			break;
@@ -1450,7 +1450,7 @@ void Main::handle_event(XEvent &ev) {
 		if (grabber->is_event(ev.type, Grabber::DOWN)) {
 			XDeviceButtonEvent* bev = (XDeviceButtonEvent *)&ev;
 			if (verbosity >= 3)
-				printf("Button %d pressed (Xi): (%d, %d, %d, %d, %d)\n", bev->button, bev->x, bev->y, bev->axis_data[0], bev->axis_data[1], bev->axis_data[2]);
+				printf("Press (Xi): %d (%ld)\n", bev->button, bev->time);
 			xinput_pressed.insert(bev->button);
 			float x, y;
 			translate_coordinates(bev->deviceid, bev->x, bev->y, bev->axis_data, x, y);
@@ -1467,7 +1467,7 @@ void Main::handle_event(XEvent &ev) {
 		if (grabber->is_event(ev.type, Grabber::UP)) {
 			XDeviceButtonEvent* bev = (XDeviceButtonEvent *)&ev;
 			if (verbosity >= 3)
-				printf("Button %d released (Xi): (%d, %d, %d, %d, %d)\n", bev->button, bev->x, bev->y, bev->axis_data[0], bev->axis_data[1], bev->axis_data[2]);
+				printf("Release (Xi): %d\n", bev->button);
 			float x, y;
 			translate_coordinates(bev->deviceid, bev->x, bev->y, bev->axis_data, x, y);
 			if (last_type == ButtonRelease && last_e->t == bev->time && last_button == bev->button) {
