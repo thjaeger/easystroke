dbd4a78cf40a7eb630d2149b8499b643c097ec7e
Thomas Jaeger <ThJaeger@gmail.com>
2008-10-02 17:24:50 -0400

    fix nautilus not opening a window when easystroke is started with the session

--- main.cc
+++ main.cc
@@ -1084,6 +1084,7 @@ Main::Main(int argc, char **argv) : kit(0) {
 	}
 	display = parse_args_and_init_gtk(argc, argv);
 	create_config_dir();
+	unsetenv("DESKTOP_AUTOSTART_ID");
 
 	signal(SIGINT, &quit);
 	signal(SIGCHLD, SIG_IGN);
