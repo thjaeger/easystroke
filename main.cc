25ac81cf5cf486d514c038994295a48413075a9a
Thomas Jaeger <ThJaeger@gmail.com>
2009-01-03 11:45:26 -0500

    foo

--- main.cc
+++ main.cc
@@ -15,7 +15,6 @@
  */
 #include <gtkmm.h>
 #include <glibmm/i18n.h>
-#include <set>
 #include <signal.h>
 #include <string>
 #include <string.h>
@@ -37,161 +36,77 @@ public:
 	enum State { NONE, BUTTON };
 	static const char *state_name[6];
 	enum EventType { DOWN = 0, UP = 1, MOTION = 2, BUTTON_MOTION = 3, PROX_IN = 4, PROX_OUT = 5 };
-	bool xinput;
 	bool is_event(int, EventType);
 
-	struct XiDevice {
-		std::string name;
-		XDevice *dev;
-		XEventClass events[6];
-		int event_type[6];
-		int all_events_n;
-		void fake_press(int b, int core);
-		void fake_release(int b, int core);
-	};
-
-	XiDevice *xi_dev;
-
-	unsigned int get_device_button_state(XiDevice *&dev);
-	XiDevice *get_xi_dev(XID id);
-
-	int nMajor;
-private:
+	XDevice *dev;
+	XEventClass events[6];
+	int event_type[6];
+	int all_events_n;
 	int button_events_n;
-	bool init_xi();
 
 	State current, grabbed;
-	bool xi_grabbed;
 	bool xi_devs_grabbed;
 
 	void set();
-	void grab_xi(bool);
 	void grab_xi_devs(bool);
-public:
 	Grabber();
 
-	void fake_button(int b);
 	void grab(State s) { current = s; set(); }
-	bool update_device_list();
 };
 
-
-bool no_xi = false;
 Grabber *grabber = 0;
-
-unsigned int ignore_mods[4] = { LockMask, Mod2Mask, LockMask | Mod2Mask, 0 };
 
 const char *Grabber::state_name[6] = { "None", "Button", "All (Sync)", "All (Async)", "Scroll", "Select" };
 
 Grabber::Grabber() {
 	current = BUTTON;
 	grabbed = NONE;
-	xi_grabbed = false;
 	xi_devs_grabbed = false;
-	xinput = init_xi();
-}
-
-float rescaleValuatorAxis(int coord, int fmin, int fmax, int tmax) {
-	if (fmin >= fmax)
-		return coord;
-	return ((float)(coord - fmin)) * (tmax + 1) / (fmax - fmin + 1);
-}
-
-bool Grabber::init_xi() {
 	button_events_n = 3;
-	if (no_xi)
-		return false;
-	int nFEV, nFER;
-	if (!XQueryExtension(dpy,INAME,&nMajor,&nFEV,&nFER))
-		return false;
-
-	if (!update_device_list())
-		return false;
-
-	return xi_dev;
-}
-
-bool Grabber::update_device_list() {
 	int n;
 	XDeviceInfo *devs = XListInputDevices(dpy, &n);
 	if (!devs)
-		return false;
-
-	xi_dev = 0;
+		exit(EXIT_FAILURE);
 
 	for (int i = 0; i < n; i++) {
-		XDeviceInfo *dev = devs + i;
+		XDeviceInfo *dev_info = devs + i;
 
-		if (strcmp(dev->name, "stylus"))
+		if (strcmp(dev_info->name, "stylus"))
 			continue;
 
-		xi_dev = new XiDevice;
-
-		xi_dev->dev = XOpenDevice(dpy, dev->id);
-		if (!xi_dev->dev) {
-			printf(_("Opening Device %s failed.\n"), dev->name);
-			delete xi_dev;
-			continue;
-		}
-		xi_dev->name = dev->name;
-
-		DeviceButtonPress(xi_dev->dev, xi_dev->event_type[DOWN], xi_dev->events[DOWN]);
-		DeviceButtonRelease(xi_dev->dev, xi_dev->event_type[UP], xi_dev->events[UP]);
-		DeviceButtonMotion(xi_dev->dev, xi_dev->event_type[BUTTON_MOTION], xi_dev->events[BUTTON_MOTION]);
-		DeviceMotionNotify(xi_dev->dev, xi_dev->event_type[MOTION], xi_dev->events[MOTION]);
-
-		xi_dev->all_events_n = 4;
+		dev = XOpenDevice(dpy, dev_info->id);
+		break;
 	}
 	XFreeDeviceList(devs);
-	xi_grabbed = false;
-	set();
-	return true;
-}
+	if (!dev)
+		exit(EXIT_FAILURE);
+	DeviceButtonPress(dev, event_type[DOWN], events[DOWN]);
+	DeviceButtonRelease(dev, event_type[UP], events[UP]);
+	DeviceButtonMotion(dev, event_type[BUTTON_MOTION], events[BUTTON_MOTION]);
+	DeviceMotionNotify(dev, event_type[MOTION], events[MOTION]);
 
-Grabber::XiDevice *Grabber::get_xi_dev(XID id) {
-	if (xi_dev->dev->device_id == id)
-		return xi_dev;
-	else
-		return 0;
+	all_events_n = 4;
+	XGrabDeviceButton(dpy, dev, 1, 0, NULL, ROOT, False, button_events_n, events, GrabModeAsync, GrabModeAsync);
+	set();
 }
 
 bool Grabber::is_event(int type, EventType et) {
-	if (!xinput)
-		return false;
-	if (type == xi_dev->event_type[et])
+	if (type == event_type[et])
 		return true;
 	return false;
-}
-
-void Grabber::grab_xi(bool grab) {
-	if (!xinput)
-		return;
-	if (!xi_grabbed == !grab)
-		return;
-	xi_grabbed = grab;
-	if (grab) {
-		XGrabDeviceButton(dpy, xi_dev->dev, 1, 0, NULL,
-				ROOT, False, button_events_n, xi_dev->events,
-				GrabModeAsync, GrabModeAsync);
-	} else {
-		XUngrabDeviceButton(dpy, xi_dev->dev, 1, 0, NULL, ROOT);
-	}
 }
 
 void Grabber::grab_xi_devs(bool grab) {
 	if (!xi_devs_grabbed == !grab)
 		return;
 	xi_devs_grabbed = grab;
-	if (grab) {
-		XGrabDevice(dpy, xi_dev->dev, ROOT, False,
-					xi_dev->all_events_n,
-					xi_dev->events, GrabModeAsync, GrabModeAsync, CurrentTime);
-	} else
-		XUngrabDevice(dpy, xi_dev->dev, CurrentTime);
+	if (grab)
+		XGrabDevice(dpy, dev, ROOT, False, all_events_n, events, GrabModeAsync, GrabModeAsync, CurrentTime);
+	else
+		XUngrabDevice(dpy, dev, CurrentTime);
 }
 
 void Grabber::set() {
-	grab_xi(true);
 	grab_xi_devs(current == NONE);
 	State old = grabbed;
 	grabbed = current;
@@ -210,19 +125,10 @@ void Grabber::set() {
 	}
 }
 
-void Grabber::XiDevice::fake_press(int b, int core) {
-	XTestFakeDeviceButtonEvent(dpy, dev, b, True,  0, 0, 0);
-}
-void Grabber::XiDevice::fake_release(int b, int core) {
-	XTestFakeDeviceButtonEvent(dpy, dev, b, False, 0, 0, 0);
-}
-
 int verbosity = 3;
 
 Display *dpy;
 
-Grabber::XiDevice *current_dev = 0;
-std::set<guint> xinput_pressed;
 bool dead = false;
 
 class Handler;
@@ -341,11 +247,6 @@ bool handle(Glib::IOCondition) {
 			if (verbosity >= 3)
 				printf("Press (Xi): %d (%d, %d, %d, %d, %d) at t = %ld\n",bev->button, bev->x, bev->y,
 						bev->axis_data[0], bev->axis_data[1], bev->axis_data[2], bev->time);
-			if (xinput_pressed.size())
-				if (!current_dev || current_dev->dev->device_id != bev->deviceid)
-					continue;
-			current_dev = grabber->get_xi_dev(bev->deviceid);
-			xinput_pressed.insert(bev->button);
 			handler->top()->press();
 		}
 		if (grabber->is_event(ev.type, Grabber::UP)) {
@@ -353,9 +254,6 @@ bool handle(Glib::IOCondition) {
 			if (verbosity >= 3)
 				printf("Release (Xi): %d (%d, %d, %d, %d, %d)\n", bev->button, bev->x, bev->y,
 						bev->axis_data[0], bev->axis_data[1], bev->axis_data[2]);
-			if (!current_dev || current_dev->dev->device_id != bev->deviceid)
-				continue;
-			xinput_pressed.erase(bev->button);
 			handler->top()->release();
 		}
 	}
