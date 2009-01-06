d09a1b5ff16d8a8b0b9f6672aff91b7bc3814434
Thomas Jaeger <ThJaeger@gmail.com>
2009-01-06 01:20:06 -0500

    Update copyright

--- actiondb.h
+++ actiondb.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, Thomas Jaeger <ThJaeger@gmail.com>
+ * Copyright (c) 2008-2009, Thomas Jaeger <ThJaeger@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
