dea9544ea2e02294ffd7340f35b7a3b3736683da
berkeleybross@gmail.com <berkeleybross@gmail.com>
2020-12-31 11:22:51 +0000

    Removed copyright notices, used #pragma once instead of verbose ifndef

--- service/gesture.h
+++ service/gesture.h
@@ -1,20 +1,4 @@
-/*
- * Copyright (c) 2008-2009, Thomas Jaeger <ThJaeger@gmail.com>
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
- * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-#ifndef __GESTURE_H__
-#define __GESTURE_H__
+#pragma once
 
 #include "stroke.h"
 #include <gdkmm.h>
@@ -121,4 +105,3 @@ public:
 	void add(RTriple p) { push_back(p); }
 	bool valid() const { return size() > 2; }
 };
-#endif
